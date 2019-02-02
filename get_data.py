283184811e194560ee23c62fefe21b14c0b4c7f4
Jeana Choi <jeanachoi@Jeanas-MacBook-Pro.local>
2019-02-02 18:31:39 -0500

    added tester file to post/get from database

---
+++ get_data.py
@@ -0,0 +1,5 @@
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://translation-bf31b.firebaseio.com', None)
+# result = firebase.post('/users', 'jeanachoi', {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
+# result = firebase.post('/users', 'jeanachoi')
+print(result)
